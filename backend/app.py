11a4a7782c1003429c40ea777e67fc2d3ab4837f
Himela2905 <himelabiswas@gmail.com>
2026-05-27 19:58:45 +0530

    resolved login timer

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
+from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
@@ -19,6 +20,7 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['JWT_SECRET_KEY']          = os.getenv('JWT_SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
 
 # Initialize extensions
 db.init_app(app)
